7c67cc2a534c2acee3e098f4ec412d3b408f940c
shreyaspatil <patil.shreyas@northeastern.edu>
2024-07-26 10:17:33 -0400

    added resume, updated metadata

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -179,7 +179,7 @@ export function LandingPage() {
                     GitHub
                   </Link>
                   <Link
-                    href="#"
+                    href="https://drive.google.com/file/d/1zAdiC9Nh-hz0jl2Jg0NTxDUCekaOjr6N/view?usp=sharing"
                     target="_blank"
                     className="inline-flex items-center justify-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                     prefetch={false}
